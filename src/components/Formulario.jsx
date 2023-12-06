2dd6dab8799fb6dd8e674b7160c46107159ff334
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-12-06 19:55:13 -0300

    Update Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,27 +1,47 @@
+import {useState} from 'react'; 
 
 const Formulario = () => {
+
+
+    const [todo,setTodo] = useState({
+
+        title : 'Todo #01',
+        description : 'Descripción Todo #01',
+        priority: true,
+        state : 'complete'
+
+    }); 
+
+    
+    const {title,description,priority,state} = todo; // object destructuring
 
     const handleSubmit = (e) => {
 
         e.preventDefault();
+        // console.log('me diste click');        
 
+        console.log(title, description, priority, state);
 
     };
 
-    // const handleChange = (e) => {
+    const handleChange = (e) => {
 
-    //         const [name]
+        const{name,type,}= e.target;
+        console.log(name);
+
+        setTodo({ ...todo, [name]: e.target.value })
 
 
-    // };
+
+    };
 
 
     return(
 
         <form onSubmit={handleSubmit}>
 
-            <input type="text" name="title" className="form-control mb-2" placeholder="Ingrese ToDo" />
-            <textarea name="description" className="form-control mb-2" placeholder="Ingrese descripción de la tarea"></textarea>
+            <input type="text" name="title" className="form-control mb-2" placeholder="Ingrese ToDo" value={title} onChange={handleChange}/>
+            <textarea name="description" className="form-control mb-2" placeholder="Ingrese descripción de la tarea" value={description} onChange={handleChange}></textarea>
             <div>
                 <input type="checkbox" name="priority" className="form-check-input mb-2 mx-1" id="inputCheck"/>
                 <label htmlFor="inputCheck"> Dar prioridad</label>
