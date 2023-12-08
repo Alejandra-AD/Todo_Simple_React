cff5c54de522bf6915199ead43d04fcadabf6cbe
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-12-08 11:20:44 -0300

    creando componente todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import Formulario from "./components/Formulario"; 
+import Todos from "./components/Todos";
+
+
+
+const initialTodos = [//datos de ejemplo
+  {
+    id:1,
+    title: 'Todo #01',
+    description: 'Descripción #01',
+    priority: true,
+    state: true
+  },
+  {
+    id:2,
+    title: 'Todo #02',
+    description: 'Descripción #02',
+    priority: false,
+    state: true
+  },
+  {
+    id:3,
+    title: 'Todo #03',
+    description: 'Descripción #03',
+    priority: false,
+    state: false
+  },
+];
+
 
 const App = () => {
-  return (
 
+  const [todos, setTodo]= useState(initialTodos);
+
+  return (
     <>
-      <div className="container mt-2">
+      <div className="container mb-2">
         
         <h1>ToDo App</h1>
         <Formulario/>
+        <Todos todos = {todos}/>
         
         
         </div>
---
+++ src/components/Todos.jsx
@@ -0,0 +1,22 @@
+
+const Todos = ({ todos }) => {
+
+    return (
+
+        <div>
+            <h2 className="text-center mb-5">Todos</h2>
+
+            <ul>
+                {todos.map(todo => (//iterando el array de objetos con map
+                    <li key={todo.id}>{todo.title}</li> //recordar siempre pasar la key
+                ))}
+            </ul>
+
+        </div>
+
+    )
+
+
+}
+
+export default Todos;
