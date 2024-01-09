ffef877b1863944893c1c63a76105379f5d34136
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2024-01-08 22:44:03 -0300

    modificando componente todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,22 @@
 
-const Todo = (todo) => {
+const Todo = ({ todo }) => {
 
-    const {id,title,description,state,priority}= todo;
+    const { id, title, description, state, priority } = todo;
 
-    return(
-    <div>
-        <li className="list-group-item my-3" key={{id}}>
-        <div><h3>{title}</h3></div>
-        <div><h5>{description}</h5></div>
-        <button className="btn btn-primary">{state}</button> 
+    return (
+
+        <li className="list-group-item my-3">
+            <div>
+                <h3>{title}</h3>
+                <h5>{description}</h5>
+                <div className="d-flex gap-2"> 
+                    <button className="btn btn-sm btn-danger">Eliminar</button>
+                    <button className="btn btn-sm btn-warning">Actualizar</button>
+                </div>
+            </div>
+
         </li>
-        
-        
-    </div>
+
 
     )
 
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -5,20 +5,16 @@ const Todos = ({ todos }) => {
 
     return (
 
-        <div>
-            <h2 className="text-center mb-5">Todos</h2>
-
-            <div className='list-group'>
-                <ul>
-                {todos.map(todo => (//iterando el array de objetos con map
-                    // <li key={todo.id}>{todo.title}</li> //recordar siempre pasar la key
-                    <Todo key={todo.id} title={todo.title} description={todo.description}/>
-                ))}
-            </ul>
+            <div className="mt-5">
+                <h2 className="text-center mb-5">Todos</h2>
+                <ul className='list-group'>
+                    {todos.map(todo => (//iterando el array de objetos con map
+                        // <li key={todo.id}>{todo.title}</li> //recordar siempre pasar la key
+                        <Todo key={todo.id} todo={todo} />
+                    ))}
+                </ul>
             </div>
 
-
-        </div>
 
     )
 
